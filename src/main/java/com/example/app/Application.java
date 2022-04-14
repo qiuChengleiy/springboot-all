23953056d9822ca4e7e47de82f73793c98f9304d
qcl <qcl@qq.com>
2022-04-14 12:32:12 +0800

    基础五

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -1,7 +1,6 @@
 package com.example.app;
 
 import com.example.app.config.AppConfig;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
